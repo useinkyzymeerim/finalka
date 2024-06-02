620936814ab2f3e5652517239269733b6aef92f3
UseinkyzyMeerim <149767355+UseinkyzyMeerim@users.noreply.github.com>
2024-06-02 23:25:30 +0600

    поменяла название сущност продукт

---
+++ src/main/resources/db/V23__CreateProducts.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS products_for_recipes CASCADE;
+
+CREATE SEQUENCE products_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE products (
+                              id BIGINT NOT NULL DEFAULT nextval('products_for_recipes_seq'),
+                          quantity INTEGER,
+                          created_at TIMESTAMP(6),
+                          deleted_at TIMESTAMP(6),
+                          last_updated_at TIMESTAMP(6),
+                          created_by VARCHAR(255),
+                          deleted_by VARCHAR(255),
+                          last_updated_by VARCHAR(255),
+                          product_name VARCHAR(255),
+                          units_enum VARCHAR(255) CHECK (units_enum IN ('gram','milliliter','litre','teaspoon','spoon','Pieces')),
+                          PRIMARY KEY (id)
+);
