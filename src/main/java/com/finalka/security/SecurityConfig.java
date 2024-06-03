4bd92a79e60fa9a206bc416e89ac8a4f49d8a887
UseinkyzyMeerim <149767355+UseinkyzyMeerim@users.noreply.github.com>
2024-06-03 19:59:04 +0600

    добавили корс проверку на корс

--- src/main/java/com/finalka/security/SecurityConfig.java
+++ src/main/java/com/finalka/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000/", "https://magicmenu-22e6224ad3f4.herokuapp.com"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000/", "http://localhost:3000/", "https://magicmenu-22e6224ad3f4.herokuapp.com"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method",
                 "Access-Control-Request-Headers"));
